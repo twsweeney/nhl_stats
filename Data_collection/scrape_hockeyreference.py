7d775de1a4802e337a770455de064a75cf3c4210
Thomas Sweeney <twsweeney13@gmail.com>
2024-01-27 12:55:57 -0800

    Changed directories around, added team data

--- Data_collection/scrape_hockeyreference.py
+++ Data_collection/scrape_hockeyreference.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np 
 import time
 from unidecode import unidecode
+from datetime import datetime
+
 
 
 
@@ -22,13 +24,14 @@ def get_team_df(team:str) -> pd.DataFrame:
     url = 'https://www.hockey-reference.com/teams/'+ team + '/2024.html'
 
 
-    patterns_to_match = ['Scoring Regular Season', 'Roster']
+    patterns_to_match = ['Team Statistics','Scoring Regular Season', 'Roster']
     combined_pattern = "|".join(patterns_to_match)
 
     tables = pd.read_html(url, match=combined_pattern)
 
-    roster_df = tables[0]
-    scoring_df = tables[1]
+    team_stats_df = tables[0]
+    roster_df = tables[1]
+    scoring_df = tables[2]
     
 
 
@@ -73,7 +76,7 @@ def get_team_df(team:str) -> pd.DataFrame:
     merged_df['Team'] = team
 
 
-    return merged_df
+    return merged_df, team_stats_df
 
 
 
@@ -92,21 +95,35 @@ def main():
               {'file_path':'data/temp_scrapes/hr_metro_data.csv', 'teams':Metro, 'label':'Metro'},
               {'file_path':'data/temp_scrapes/hr_central_data.csv', 'teams':Central, 'label':'Central'},
               {'file_path':'data/temp_scrapes/hr_pacific_data.csv', 'teams':Pacific, 'label':'Pacific'}]
+    
+
+
+    current_datetime =  datetime.now()
+    current_date_str = str(current_datetime.date()).replace('-','_')
+    team_stats_output_df = pd.DataFrame()
+    team_stats_output_directory = 'data/processed/team_data/'
+    team_stats_output_path = team_stats_output_directory + current_date_str + '_team_stats_data.csv'
 
     for index, division_dict in enumerate(league):
-        print(f'Starting scrape on: {division_dict['label']}')
+        print(f'Starting scrape on: {division_dict["label"]}')
         teams = division_dict['teams']
         for i in range(len(teams)):
             if i == 0:
-                league_df = get_team_df(teams[i])
+                league_df, team_stats_temp  = get_team_df(teams[i])
+                team_stats_output_df = pd.concat([team_stats_output_df, team_stats_temp])
             else:
-                temp_df = get_team_df(teams[i])
+                temp_df, team_stats_temp = get_team_df(teams[i])
                 league_df = pd.concat([league_df, temp_df])
+                team_stats_output_df = pd.concat([team_stats_output_df, team_stats_temp])
+
         league_df.to_csv(division_dict['file_path'])
-        print(f'{division_dict['label']} scrape complete. Waiting 60 seconds now')
+        print(f'{division_dict["label"]} scrape complete. Waiting 60 seconds now')
         if index != 3:
             time.sleep(60)
         else:
+            team_stats_clean_output_df = team_stats_output_df.drop_duplicates().reset_index(drop=True)
+
+            team_stats_clean_output_df.to_csv(team_stats_output_path)
             print('Complete!')
 
         
